8f23cb64e88363b421f0d96646e88d3fd0469a6b
Alexandr Rybalka <polterageist@gmail.com>
2017-03-29 05:07:42 +0300

    Сделал плейсхолдеры для сервера и клиента

---
+++ Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
+
+namespace TestEchoServer {
+    class Client {
+        public Client(string room, string id, IPEndPoint endpoint) {
+            Room = room;
+            Id = id;
+            ConnectEndPoint = endpoint;
+        }
+
+        public string Room { get; }
+
+        public string Id { get; }
+
+        public IPEndPoint ConnectEndPoint { get; }
+        
+        public void Run() {
+
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestEchoServer {
+    class Server {
+        public Server(IPEndPoint listenEndPoint) {
+            ListenEndPoint = listenEndPoint;
+        }
+
+        public IPEndPoint ListenEndPoint { get; }
+
+        public void Run() {
+
+        }
+    }
+}
